1a4a349647be747388a35a8c4d438b2c0078f50d
Rajat Sharma <50054554+rajatxs@users.noreply.github.com>
2023-09-25 18:41:31 +0530

    define CreatePostPayload struct

--- types/post.go
+++ types/post.go
@@ -7,3 +7,18 @@ type GetPostsMetadataOptions struct {
 	Limit   int64  `json:"limit"`
 	Skip    int64  `json:"skip"`
 }
+
+type CreatePostPayload struct {
+	Title             string   `json:"title"`
+	Slug              string   `json:"slug"`
+	Desc              string   `json:"desc"`
+	Tags              []string `json:"tags"`
+	Topic             string   `json:"topic"`
+	Body              string   `json:"body"`
+	Public            bool     `json:"public"`
+	CoverImageId      string   `json:"coverImageId"`
+	CoverImagePath    string   `json:"coverImagePath"`
+	CoverImageRefName string   `json:"coverImageRefName"`
+	CoverImageRefUrl  string   `json:"coverImageRefUrl"`
+	AuthorId          string   `json:"authorId"`
+}
