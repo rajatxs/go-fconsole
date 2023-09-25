ee383a20ba8a1aa20cf41d2846092e437a430cf3
Rajat Sharma <50054554+rajatxs@users.noreply.github.com>
2023-09-25 13:01:58 +0530

    define App types

--- app.go
+++ app.go
@@ -9,10 +9,7 @@ import (
 
 	"github.com/rajatxs/go-fconsole/config"
 	"github.com/rajatxs/go-fconsole/db"
-	"github.com/rajatxs/go-fconsole/models"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/rajatxs/go-fconsole/types"
 )
 
 // App struct
@@ -45,15 +42,7 @@ func (a *App) terminate(ctx context.Context) {
 	}
 }
 
-func (a *App) GetAppConfigVariables() map[string]string {
-	var env = map[string]string{}
-
-	env["ENV"] = config.Env()
-	env["ADMIN_ID"] = config.AdminId()
-	env["CLOUDINARY_ID"] = config.CloudinaryId()
-	return env
-}
-
+// Opens given url into default browser
 func (a *App) OpenBrowser(url string, newTab bool) error {
 	var cmd *exec.Cmd
 
@@ -69,89 +58,33 @@ func (a *App) OpenBrowser(url string, newTab bool) error {
 	return cmd.Start()
 }
 
-func (a *App) GetVersions() map[string]string {
-	var ver = map[string]string{}
+// Returns public app config variables
+func (a *App) GetAppConfigVariables() (env *types.AppPublicConfigVariables) {
+	env = &types.AppPublicConfigVariables{}
+
+	env.ENV = config.Env()
+	env.ADMIN_ID = config.AdminId()
+	env.CLOUDINARY_ID = config.CloudinaryId()
+	return env
+}
+
+// Returns app version information
+func (a *App) GetVersions() (ver *types.AppVersions) {
+	ver = &types.AppVersions{}
 
 	currentUser, err := user.Current()
 	if err != nil {
 		log.Println(err)
 	}
 
-	ver["app"] = "0.0.1"
-	ver["date"] = "2023-09-20T07:53:14.501Z"
-	ver["wails"] = "2.6.0"
-	ver["go"] = "1.20.3"
-
-	// TODO: Write "WebView2 Runtime Version" in UI
-	ver["webview2"] = "111.0.2045.32"
-	ver["os"] = runtime.GOOS
-	ver["arch"] = runtime.GOARCH
-	ver["uname"] = currentUser.Name
-	ver["homedir"] = currentUser.HomeDir
+	ver.App = "0.0.1"
+	ver.Date = "2023-09-20T07:53:14.501Z"
+	ver.Wails = "2.6.0"
+	ver.Go = "1.20.3"
+	ver.WebView2 = "111.0.2045.32"
+	ver.Os = runtime.GOOS
+	ver.Arch = runtime.GOARCH
+	ver.Username = currentUser.Name
+	ver.HomeDir = currentUser.HomeDir
 	return ver
-}
-
-func (a *App) GetPostsMetadata(scope string, topic string, sortBy string, limit int64, skip int64) ([]models.PostMetadataDocument, error) {
-	var posts []models.PostMetadataDocument
-	var collName string
-	var filter primitive.D = bson.D{}
-
-	opts := options.Find()
-
-	if scope == "public" {
-		collName = "publicPostsMetadata"
-	} else {
-		collName = "privatePostsMetadata"
-	}
-
-	if topic != "all" {
-		filter = bson.D{{Key: "topic", Value: topic}}
-	}
-
-	switch sortBy {
-	case "title":
-		opts.SetSort(map[string]int{"title": 1})
-
-	case "topic":
-		opts.SetSort(map[string]int{"topic": 1})
-
-	case "newest":
-		opts.SetSort(map[string]int{"createdAt": -1})
-
-	case "oldest":
-		opts.SetSort(map[string]int{"createdAt": 1})
-
-	case "updated":
-		opts.SetSort(map[string]int{"updatedAt": -1})
-	}
-
-	opts.SetLimit(limit)
-	opts.SetSkip(skip)
-
-	if cur, err := db.MongoDb().Collection(collName).Find(a.ctx, filter, nil, opts); err != nil {
-		log.Println(err)
-	} else {
-		defer cur.Close(a.ctx)
-
-		for cur.Next(a.ctx) {
-			var post models.PostMetadataDocument
-
-			if err = cur.Decode(&post); err != nil {
-				return nil, err
-			}
-			posts = append(posts, post)
-		}
-
-		if err = cur.Err(); err != nil {
-			return nil, err
-		}
-	}
-
-	return posts, nil
-}
-
-func (a *App) GetPostCount(scope string, includeDeleted bool) (int64, error) {
-	return db.MongoDb().Collection("posts").CountDocuments(
-		a.ctx,
-		bson.D{{Key: "public", Value: scope == "public"}, {Key: "deleted", Value: includeDeleted}})
 }
