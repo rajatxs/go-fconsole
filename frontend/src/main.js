2347f4bc0c307b13b5aa164ee268856104f00634
Rajat Sharma <50054554+rajatxs@users.noreply.github.com>
2023-09-27 13:16:54 +0530

    update theme config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -25,18 +25,36 @@ const vuetify = createVuetify({
       defaultTheme: getString('theme', 'light'),
       themes: {
          light: {
-            dark: false,
-            colors: {
-              primary: '#00695f',
-              secondary: '#ffea00'
-            }
+           dark: false,
+           colors: {
+              primary: '#6200EE',
+              'primary-lighten-1': '#7e3ff2',
+              'primary-lighten-2': '#9965f4',
+              'primary-lighten-3': '#b794f6',
+              'primary-lighten-4': '#d4bff9',
+              'primary-lighten-5': '#efe5fd',
+
+              'primary-darken-1': '#5300e8',
+              'primary-darken-2': '#3d00e0',
+              'primary-darken-3': '#1c00db',
+              'primary-darken-4': '#0000d6',
+           },
          },
          dark: {
             dark: true,
             colors: {
-              primary: '#009688',
-              secondary: '#ffea00'
-            }
+               primary: '#5300e8',
+               'primary-lighten-1': '#5300e8',
+               'primary-lighten-2': '#3d00e0',
+               'primary-lighten-3': '#1c00db',
+               'primary-lighten-4': '#0000d6',
+               'primary-lighten-5': '#6200EE',
+
+               'primary-darken-1': '#7e3ff2',
+               'primary-darken-2': '#9965f4',
+               'primary-darken-3': '#b794f6',
+               'primary-darken-4': '#d4bff9',
+            },
          },
       },
    },
