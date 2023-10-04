3bd452cc5bbb9aa1116e30c0dc22ea7b53a091db
Rajat Sharma <50054554+rajatxs@users.noreply.github.com>
2023-10-04 16:18:29 +0530

    update setMetadata()

--- frontend/src/components/PostEditor/store.js
+++ frontend/src/components/PostEditor/store.js
@@ -32,6 +32,26 @@ export const state = reactive({
    coverImageAssetId: '',
 });
 
+/**
+ * Sets state properties from given `data` payload
+ * @param {import('../../../wailsjs/go/models').models.PostDocument} data 
+ */
+export function setMetadata(data) {
+   state.title = data.title;
+   state.slug = data.slug;
+   state.topic = data.topic;
+   state.desc = data.desc;
+   state.tags = data.tags;
+   state.publicScope = data.public;
+
+   if (data.coverImage) {
+      state.coverImageRefName = data.coverImage.refName;
+      state.coverImageRefUrl = data.coverImage.refUrl;
+      state.coverImagePublicId = data.coverImage.path;
+      state.coverImageAssetId = data.coverImage.id;
+   }
+}
+
 /** Sets default values to metadata reactive properties */
 export function clearMetadata() {
    state.title = '';
