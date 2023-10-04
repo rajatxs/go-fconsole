ba050120e3017eb26762adfcd0e37ace0947c049
Rajat Sharma <50054554+rajatxs@users.noreply.github.com>
2023-10-04 15:41:32 +0530

    define PostDocument model

--- models/post.go
+++ models/post.go
@@ -3,6 +3,7 @@ package models
 import (
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -26,4 +27,23 @@ type PostMetadataDocument struct {
 	CreatedAt  time.Time          `bson:"createdAt" json:"createdAt"`
 	UpdatedAt  time.Time          `bson:"updatedAt" json:"updatedAt"`
 	CoverImage *PostCoverImage    `bson:"coverImage" json:"coverImage"`
+}
+
+type PostDocument struct {
+	Id         primitive.ObjectID `bson:"_id" json:"_id"`
+	Title      string             `bson:"title" json:"title"`
+	Slug       string             `bson:"slug" json:"slug"`
+	Desc       string             `bson:"desc" json:"desc"`
+	Tags       []string           `bson:"tags" json:"tags"`
+	Topic      string             `bson:"topic" json:"topic"`
+	Body       bson.M             `bson:"body" json:"body"`
+	Format     string             `bson:"format" json:"format"`
+	Stars      int64              `bson:"stars" json:"stars"`
+	Public     bool               `bson:"public" json:"public"`
+	Deleted    bool               `bson:"deleted" json:"deleted"`
+	CoverImage *PostCoverImage    `bson:"coverImage" json:"coverImage"`
+	AuthorId   primitive.ObjectID `bson:"authorId" json:"authorId"`
+	Related    []string           `bson:"related" json:"related"`
+	CreatedAt  time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt  time.Time          `bson:"updatedAt" json:"updatedAt"`
 }
