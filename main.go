c747c589a1b6540ace4fe2fe51757c8d2bf8062f
Rajat Sharma <50054554+rajatxs@users.noreply.github.com>
2023-10-11 12:36:23 +0530

    define preconfig()

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"embed"
+	"log"
+	"os"
 
+	"github.com/rajatxs/go-fconsole/config"
 	"github.com/rajatxs/go-fconsole/services"
 	"github.com/rajatxs/go-fconsole/util"
 	"github.com/wailsapp/wails/v2"
@@ -15,6 +18,28 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
+func preconfig() error {
+	var err error
+
+	// parse default config
+	if err = config.Parse(); err != nil {
+		log.Fatal(err)
+	}
+
+	// create the root configuration directory if it does not exist
+	configDir := config.RootDir()
+	_, err = os.Stat(configDir)
+
+	if os.IsNotExist(err) {
+		if err = os.Mkdir(configDir, 0755); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	util.InitLogger()
+	return err
+}
+
 func runApp() error {
 	// Create an instance of the app structure
 	app := NewApp()
@@ -24,7 +49,6 @@ func runApp() error {
 	topicService := services.NewTopicService()
 
 	// Create application with options
-	// TODO: Add `backgroundColor` property
 	err := wails.Run(&options.App{
 		Title:         "Console",
 		Width:         1300,
@@ -60,5 +84,6 @@ func runApp() error {
 }
 
 func main() {
+	util.Attempt(preconfig())
 	util.Attempt(runApp())
 }
