13c8bc94e97185735347dc12f5a5c15a3bb56fed
Rajat Sharma <50054554+rajatxs@users.noreply.github.com>
2023-10-13 22:55:41 +0530

    created PostSelector component

---
+++ frontend/src/components/PostSelector/index.vue
@@ -0,0 +1,224 @@
+<script setup>
+import { defineProps, defineEmits, ref, watch, onBeforeUnmount, computed } from 'vue';
+import { GetPostsMetadata } from '../../../wailsjs/go/services/PostService';
+import { GetPublicTopics } from '../../../wailsjs/go/services/TopicService';
+import { formatTime, getPostCoverImageURL } from '../../utils';
+
+/** @type {Record<string, any>} */
+let publicTopics = {};
+
+const props = defineProps({
+   visible: {
+      type: Boolean,
+      default: false,
+   },
+   topic: {
+      type: String,
+      required: false,
+   },
+});
+const emit = defineEmits(['close', 'select']);
+
+/** @type {import('vue').Ref<{title: string, value: string}[]>} */
+const topicOptions = ref([]);
+
+/** @type {import('vue').Ref<{title: string, value: string}[]>} */
+const sortOptions = ref([
+   {
+      title: 'Title',
+      value: 'title',
+   },
+   {
+      title: 'Topic',
+      value: 'topic',
+   },
+   {
+      title: 'Newest',
+      value: 'newest',
+   },
+   {
+      title: 'Oldest',
+      value: 'oldest',
+   },
+   {
+      title: 'Last Updated',
+      value: 'updated',
+   },
+]);
+
+/** @type {import('vue').Ref<boolean>} */
+const fetchPostError = ref(false);
+
+/** @type {import('vue').Ref<boolean>} */
+const fetchTopicError = ref(false);
+
+/** @type {import('vue').Ref<string>} */
+const topic = ref('all');
+
+/** @type {import('vue').Ref<string>} */
+const sort = ref('updated');
+
+/** @type {import('vue').Ref<any[]>} */
+const posts = ref([]);
+
+const postsList = computed(function() {
+   let _list = [];
+
+   posts.value.forEach((_md, _mdindex) => {
+      if (_mdindex > 0 && _mdindex < posts.value.length) {
+         _list.push({ type: 'divider', inset: true });
+      }
+      _list.push(_md);
+   });
+
+   return _list;
+});
+
+/** @param {string} id  */
+function topicName(id) {
+   if (publicTopics[id]) {
+      return publicTopics[id].name;
+   } else {
+      return 'Other';
+   }
+}
+
+async function fetchPostsMetadata() {
+   try {
+      const _posts = await GetPostsMetadata({
+         private: false,
+         topic: topic.value,
+         sortBy: sort.value,
+         limit: 12,
+         skip: 0,
+      });
+
+      posts.value = _posts;
+   } catch (error) {
+      console.error(error);
+      fetchPostError.value = true;
+   }
+}
+
+/** @param {any} $event */
+function onPostSelect($event) {
+   const id = $event.id;
+   const post = posts.value.find(_post => _post._id === id);
+
+   if (!post) {
+      return;
+   }
+
+   emit('select', post);
+}
+
+watch([topic, sort], fetchPostsMetadata);
+
+watch(() => props.visible, async function(newState) {
+   if (!newState) {
+      return;
+   }
+
+   if (props.topic) {
+      topic.value = props.topic;
+   }
+
+   try {
+      let options = [{
+         title: "All",
+         value: "all",
+      }];
+
+      publicTopics = await GetPublicTopics();
+
+      for (const topicId in publicTopics) {
+         options.push({
+            title: publicTopics[topicId].name,
+            value: topicId,
+         });
+      }
+      
+      topicOptions.value = options;
+   } catch (error) {
+      console.error(error);
+      fetchTopicError.value = true;
+   }
+   
+   await fetchPostsMetadata();
+});
+
+onBeforeUnmount(function() {
+   // @ts-ignore
+   publicTopics = {};
+})
+</script>
+
+<template>
+   <v-dialog
+      v-model="props.visible"
+      scrollable
+      :width="550"
+      :height="600">
+      <v-card>
+         <v-toolbar dark>
+            <v-btn icon dark @click="emit('close')">
+               <v-icon>mdi-close</v-icon>
+            </v-btn>
+
+            <v-toolbar-title>Select Post</v-toolbar-title>
+         </v-toolbar>
+
+         <v-card-item>
+            <v-row>
+               <v-col cols="12" sm="6">
+                  <v-select 
+                     v-model="topic" 
+                     :items="topicOptions" 
+                     label="Topic">
+                  </v-select>
+               </v-col>
+               <v-col cols="12" sm="6">
+                  <v-select 
+                     v-model="sort" 
+                     :items="sortOptions" 
+                     label="Sort By">
+                  </v-select>
+               </v-col>
+            </v-row>
+         </v-card-item>
+
+         <v-card-text>
+            <v-list
+               :items="postsList"
+               item-title="title"
+               item-value="_id"
+               item-props
+               lines="2"
+               @click:select="onPostSelect">
+               <template v-slot:prepend="{ item }">
+                  <v-avatar :rounded="0">
+                     <v-img
+                        :src="getPostCoverImageURL(item.coverImage.path)"
+                        :alt="item.title">
+                     </v-img>
+                  </v-avatar>
+               </template>
+
+               <template v-slot:subtitle="{ item }">
+                  {{ topicName(item.topic) }} - {{ formatTime(item.createdAt) }}
+               </template>
+               <template v-slot:divider>
+                  <v-divider inset></v-divider>
+               </template>
+            </v-list>
+         </v-card-text>
+      </v-card>
+    </v-dialog>
+
+    <v-snackbar v-model="fetchPostError" :timeout="3000" color="error">
+      Couldn't get posts
+   </v-snackbar>
+    <v-snackbar v-model="fetchTopicError" :timeout="3000" color="error">
+      Couldn't get topics
+   </v-snackbar>
+</template>
